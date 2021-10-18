f56b9a780eae55e6e93e1180144a8a664e1766bf
Daniel Vergara <danielvsuarez01@gmail.com>
2021-10-18 09:19:25 -0500

    terminando taller  2

--- descuentos.js
+++ descuentos.js
@@ -13,3 +13,18 @@ function calcularPrecioConDescuento(precio, descuento) {
   porcentajePrecioConDescuento,
   precioConDescuento
 }) */
+
+function priceDiscount() {
+  const inputPrice = document.getElementById('inputPrice')
+  const priceValue = parseInt(inputPrice.value)
+  const inputDiscount = document.getElementById('inputDiscount')
+  const discountValue = parseInt(inputDiscount.value)
+
+  const precioConDescuento = calcularPrecioConDescuento(
+    priceValue,
+    discountValue
+  )
+
+  const resultP = document.getElementById('resultP')
+  resultP.innerText = `El precio con descuento es $${precioConDescuento}`
+}
